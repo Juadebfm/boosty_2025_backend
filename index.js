41e720a03acfaf22547812b5854be6533d10048d
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2025-07-01 09:58:16 +0100

    feat: implement cors fix

--- index.js
+++ index.js
@@ -24,7 +24,20 @@ mongoose
   .then(() => console.log("DB Connection Established"))
   .catch((err) => console.log(err));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      // Add your production frontend URL here when deployed
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-clerk-auth-token"],
+    optionsSuccessStatus: 200,
+  })
+);
 //Use the route
 app.use(express.json());
 app.use(clerkMiddleware());
